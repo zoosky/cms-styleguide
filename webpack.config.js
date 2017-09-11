101dcf8adeb3a06d422ac0dfe7ddd1e060cead64
Thomas D'Ascoli <thomas@dasco.li>
2017-09-11 11:10:56 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = function(fabricatorConfig,build) {
+
+	"use strict";
+	var dest = fabricatorConfig.dest;
+	if (build!==undefined){
+		dest = build;
+	}
+
+	var config = {
+		entry: {
+			'fabricator/scripts/f': fabricatorConfig.src.scripts.fabricator,
+			'styleguide/scripts/alv-ch_cms': fabricatorConfig.src.scripts.styleguide
+		},
+		output: {
+			path: path.resolve(__dirname, dest, 'assets'),
+			filename: '[name].js'
+		},
+		module: {
+			loaders: [
+				{
+					test: /\.js$/,
+					//exclude: /(node_modules|prism\.js)/,
+					loaders: ['babel-loader']
+				}
+			]
+		},
+		plugins: [],
+		cache: {}
+	};
+
+	if (!fabricatorConfig.dev) {
+		config.plugins.push(
+			new webpack.optimize.UglifyJsPlugin()
+		);
+	}
+
+	if (dest!=='dist') {
+		config.entry = {
+			'scripts/alv-ch': fabricatorConfig.src.scripts.styleguide,
+			'scripts/alv-ch_reset': fabricatorConfig.src.scripts.reset
+		};
+		config.output.path = path.resolve(__dirname, dest);
+	}
+
+	return config;
+
+};

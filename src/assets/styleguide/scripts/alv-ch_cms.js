ebc4384e34d8457f64243c7429218a780b4d119d
Thomas Rohrer <rohrtom@users.noreply.github.com>
2018-09-19 09:12:04 +0200

    Add translated url to language links

--- src/assets/styleguide/scripts/alv-ch_cms.js
+++ src/assets/styleguide/scripts/alv-ch_cms.js
@@ -40,6 +40,14 @@ $( document ).ready(function() {
 	var navi = $('.mod-mainnavigation').detach();
 	var md = new MobileDetect(window.navigator.userAgent);
 
+	//add work.swiss to language href to change url accordingly
+	var url_en = 'work.swiss';
+	var hrefEn = $(‘.nav-lang a[lang="en"]’).attr(‘href’); // den bestehenden Link auslesen
+	var newHref = url_en+=hrefEn;
+    //hrefEn.replace(‘arbeit.swiss’, ’work.swiss’); // den bestehenden Link anpassen: arbeit.swiss ersetzen mit zBsp: work.swiss oder wie der neue Link dann auch aussieht, dass ensprechende ersetzen
+    $(‘.nav-lang a[lang="en"]’).attr(‘href’,newHref); // den Link ersetzen mit dem neuen
+
+	
 	if (md.phone() || $(document).width()<768) {
 		$('.navbar-alv').after(navi);
 
